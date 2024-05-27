2ad2661561fc0c1cc33ba94202c3b202ad1aa75d
sinking8 <ashwinblaze111@gmail.com>
2024-05-27 13:55:46 -0700

    added updated evaluation code

--- experiments/evaluation/evaluation.py
+++ experiments/evaluation/evaluation.py
@@ -1,60 +1,47 @@
 import pandas as pd
 import numpy as np
-import networkx as nx
 
-from graspologic.embed import node2vec_embed
-from sklearn.model_selection import cross_validate
+from sklearn.metrics import f1_score,precision_score,recall_score
 from sklearn.model_selection import StratifiedKFold
-from sklearn.ensemble import RandomForestClassifier
 
-def getNodeEmbeddings(input_file, output_file):
+def evaluation_metrics(labels_map,n_splits=10):
     """
-    Computes the node embeddings (dim = 128) of each node in a given network.
-
     Args:
-        input_file: path of the graph file in .gexf extension
-        output_file: path where to save the output file
+        label_map: dataframe with the classification label for each userid (columns = ['userid', 'label' (0 or 1),'eigen_centrality])
     Returns:
-
+        Average Performance across 10 folds
     """
-    G = nx.read_gexf(input_file)
-    G = nx.Graph(G)
 
-    node2vec_embedding, node_labels = node2vec_embed(
-        G, num_walks=16, walk_length=16, workers=16, inout_hyperparameter=1.0, return_hyperparameter=1.0
-    )
-    
-    np.save(output_file+'_embed.npy', node2vec_embedding) # saves the emebddings
-    np.save(output_file+'_nodeLabels.npy', node_labels) #saves the node labels relative to the embeddings files
+    skf = StratifiedKFold(n_splits=n_splits)
+    X = labels_map[['user_id','eigen_centrality']]
+    y = labels_map['label']
+    skf.get_n_splits(X, y)
 
-def classify_embeddings(node2vec_embedding, node_labels, label_map):
-    """
-    Trains a random forest classifier on node embeddings, using 10 Fold Cross-Validation
+    metrics = {"f1_score":0,"recall":0,"precision":0}
 
-    Args:
-        node2vec_embedding:numpy array of node embeddings
-        node_labels: numpy array of node labels
-        label_map: dataframe with the classification label for each userid (columns = ['userid', 'label'])
-    Returns:
-        model: trained model
-        n_scores: 10-fold cross-validation performance
-    """
-    node_labels = node_labels.astype(str)
-    label_map = label_map[['userid', 'label']].set_index('userid').T.to_dict('list')
-      
-    node_colours = []
-    
-    for target in node_labels:
-        if target in label_map.keys():
-            node_colours.append(label_map[target][0])
-        else: 
-            node_colours.append(np.NaN)
-    
-    df = pd.DataFrame(node2vec_embedding)
-    df['label'] = node_colours
-    df.dropna(inplace=True)
-    
-    model = RandomForestClassifier(class_weight='balanced')
-    cv = StratifiedKFold(n_splits=10)
-    n_scores = cross_validate(model, df[[i for i in range(0, 128)]], df['label'], scoring=['accuracy', 'recall', 'precision','f1', 'roc_auc'], cv=cv, n_jobs=-1, error_score='raise')
-    return n_scores
+    for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        thresh = [i for i in range(1,100,2)]
+        centrality_ranges = [np.percentile(X_train['eigen_centrality'].values,t) for t in thresh]
+        max_centrality_range = -1
+        max_f1_score = -1
+
+        for cent in centrality_ranges:
+            new_y_train = X_test['eigen_centrality'].apply(lambda x: 1 if x>=cent else 0)
+            if(max_f1_score<f1_score(new_y_train,y_train)):
+                max_f1_score = f1_score(new_y_train,y_train)
+                max_centrality_range = cent
+
+        y_test_preds = X_test['eigen_centrality'].apply(lambda x: 1 if x>=max_centrality_range else 0)
+
+        metrics['f1_score']+=f1_score(y_test,y_test_preds)
+        metrics['recall']+=recall_score(y_test,y_test_preds)
+        metrics['precision']+=precision_score(y_test,y_test_preds)
+
+    metrics['f1_score']/=float(n_splits)
+    metrics['recall']/=float(n_splits)
+    metrics['precision']/=float(n_splits)
+
+    return metrics
