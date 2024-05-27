6e7c05fae97ef1bb78c32ae010d040b7a4a64764
Manita Pote <potem@iu.edu>
2024-05-27 17:48:07 -0400

    changed parameters

--- experiments/cohashtag/GenerateCoHashtagNetworks_IO.py
+++ experiments/cohashtag/GenerateCoHashtagNetworks_IO.py
@@ -22,18 +22,29 @@ if __name__ == "__main__": # Needed for parallel processing
     # config = cz.load_config("<path to config>")
 
     # Load JSON data from file
-    cohashtag_0="/N/project/INCAS/merge_network/cuba_bipartite_original_filter_0.json"
-    cohashtag_1="/N/project/INCAS/merge_network/cuba_bipartite_original_filter_10.json"
-    networksPath = "/N/project/INCAS/merge_network/cuba_cohashtag_0.xnet"
+    # cohashtag_0="/N/project/INCAS/merge_network/cuba_bipartite_original_filter_0.json"
     
-    bipartiteEdges = load_data(cohashtag_0)
+    realizations=10000
+    pvaluesQuantized=[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.25, 0.5]
+    filter_activity=10
+    
+    print('***Starting null model***\n')
+    print('Realizations :', realizations)
+    print('pvaluesQuantized :', pvaluesQuantized)
+    print('Activity filtered at :', filter_activity)
+    print('\n\n')
+    
+    cohashtag_1=f"/N/project/INCAS/merge_network/cuba_bipartite_original_filter_{filter_activity}.json"
+    networksPath = f"/N/project/INCAS/merge_network/cuba_cohashtag_{realizations}_{filter_activity}.xnet"
+    
+    bipartiteEdges = load_data(cohashtag_1)
 
     # creates a null model output from the bipartite graph
     nullModelOutput = cz.nullmodel.bipartiteNullModelSimilarity(
         bipartiteEdges,
         scoreType=["zscore","pvalue-quantized"], # pvalue-quantized, pvalue, or zscore, 
-        pvaluesQuantized=[0.001,0.01,0.05,0.1,0.25,0.5],
-        realizations=1000,
+        pvaluesQuantized=pvaluesQuantized,
+        realizations=realizations,
         idf="smoothlog", # None, "none", "linear", "smoothlinear", "log", "smoothlog"
         batchSize=100,
         workers=-1,
